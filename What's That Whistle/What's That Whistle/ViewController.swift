9bd5e97b290c1273f2e96bfcdece8d5546cd095c
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-11-05 17:12:32 +0700

    initial push.

---
+++ What's That Whistle/What's That Whistle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's That Whistle
+//
+//  Created by Yohannes Wijaya on 11/5/15.
+//  Copyright © 2015 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
